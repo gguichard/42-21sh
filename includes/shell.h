09a76e5f3f9d27f4ad126b3b9dc969903ea8471b
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-01-03 14:31:34 +0100

    WIP

--- includes/shell.h
+++ includes/shell.h
@@ -6,11 +6,28 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/03 13:33:39 by gguichar          #+#    #+#             */
-/*   Updated: 2019/01/03 13:36:04 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/01/03 14:29:15 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHELL_H
 # define SHELL_H
+
+# include "libft.h"
+
+typedef struct	s_var
+{
+	char		*key;
+	char		*value;
+}				t_var;
+
+typedef struct	s_shell
+{
+	int			argc;
+	char		**argv;
+	t_list		*env;
+	t_list		*local;
+	int			last_exit;
+}				t_shell;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/03 13:34:02 by gguichar          #+#    #+#             */
-/*   Updated: 2019/01/03 13:36:27 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/01/03 14:29:57 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 int	main(int argc, char **argv, char **environ)
 {
-	(void)argc;
-	(void)argv;
+	t_shell	shell;
+
+	shell.argc = argc;
+	shell.argv = argv;
 	(void)environ;
 	return (0);
 }
