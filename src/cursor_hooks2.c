03354cf42f6301d5e1c9f27a416df5cbbec5d3be
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-01-05 15:08:40 +0100

    Fix mot suivant

--- src/cursor_hooks2.c
+++ src/cursor_hooks2.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/05 14:19:25 by gguichar          #+#    #+#             */
-/*   Updated: 2019/01/05 14:56:03 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/01/05 15:07:56 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ void	move_cursor_next_word(t_term *term)
 		tputs(tgetstr("bl", NULL), 1, term_putchar);
 		return ;
 	}
-	while (term->cursor < term->size && term->line[term->cursor] == ' ')
-		move_cursor_right(term);
 	while (term->cursor < term->size && term->line[term->cursor] != ' ')
+		move_cursor_right(term);
+	while (term->cursor < term->size && term->line[term->cursor] == ' ')
 		move_cursor_right(term);
 }
