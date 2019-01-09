e4e98cc0856657060f76e5a80b063d6c5ddbc2da
Franck W <franck.rj.w@gmail.com>
2019-01-09 16:41:39 +0100

    Normage de split_cmd.

--- split_cmd_semicolon.c
+++ split_cmd_semicolon.c
@@ -6,7 +6,7 @@
 /*   By: fwerner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 11:20:47 by fwerner           #+#    #+#             */
-/*   Updated: 2019/01/09 15:23:58 by fwerner          ###   ########.fr       */
+/*   Updated: 2019/01/09 16:37:11 by fwerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,26 @@
 #include "str_cmd_inf.h"
 #include "split_cmd_semicolon.h"
 
+int		add_cur_cmd_to_lst(t_list **cmd_lst, t_str_cmd_inf *str_cmd_inf,
+		char *cmd_start)
+{
+	t_list	*new_elem;
+	size_t	cur_cmd_size;
+
+	cur_cmd_size = (str_cmd_inf->str + str_cmd_inf->pos)
+		- cmd_start;
+	if ((new_elem = ft_lstnew(cmd_start,
+					cur_cmd_size + 1)) == NULL)
+		return (0);
+	((char*)new_elem->content)[cur_cmd_size] = '\0';
+	ft_lstpush(cmd_lst, new_elem);
+	return (1);
+}
+
 t_list	*split_cmd_semicolon(t_str_cmd_inf *str_cmd_inf)
 {
 	t_list	*cmd_lst;
-	t_list	*new_elem;
 	char	*after_last_semicolon;
-	size_t	cur_cmd_size;
 
 	cmd_lst = NULL;
 	after_last_semicolon = str_cmd_inf->str;
@@ -30,23 +44,15 @@ t_list	*split_cmd_semicolon(t_str_cmd_inf *str_cmd_inf)
 				&& str_cmd_inf->str[str_cmd_inf->pos] == ';'
 				&& !scmd_cur_char_is_escaped(str_cmd_inf))
 		{
-			cur_cmd_size = (str_cmd_inf->str + str_cmd_inf->pos)
-				- after_last_semicolon;
-			if ((new_elem = ft_lstnew(after_last_semicolon,
-							cur_cmd_size + 1)) == NULL)
+			if (!add_cur_cmd_to_lst(&cmd_lst, str_cmd_inf,
+						after_last_semicolon))
 				return (ft_lstfree(&cmd_lst));
-			((char*)new_elem->content)[cur_cmd_size] = '\0';
-			ft_lstpush(&cmd_lst, new_elem);
 			after_last_semicolon = (str_cmd_inf->str + str_cmd_inf->pos) + 1;
 		}
 		scmd_move_to_next_char(str_cmd_inf);
 	}
-	cur_cmd_size = (str_cmd_inf->str + str_cmd_inf->pos)
-		- after_last_semicolon;
-	if ((new_elem = ft_lstnew(after_last_semicolon,
-					cur_cmd_size + 1)) == NULL)
+	if (!add_cur_cmd_to_lst(&cmd_lst, str_cmd_inf,
+				after_last_semicolon))
 		return (ft_lstfree(&cmd_lst));
-	((char*)new_elem->content)[cur_cmd_size] = '\0';
-	ft_lstpush(&cmd_lst, new_elem);
 	return (cmd_lst);
 }
--- split_cmd_semicolon.h
+++ split_cmd_semicolon.h
@@ -6,15 +6,15 @@
 /*   By: fwerner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 13:19:19 by fwerner           #+#    #+#             */
-/*   Updated: 2019/01/09 13:20:30 by fwerner          ###   ########.fr       */
+/*   Updated: 2019/01/09 16:40:08 by fwerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPLIT_CMD_SEMICOLON_H
 # define SPLIT_CMD_SEMICOLON_H
 
-#include "libft.h"
-#include "str_cmd_inf.h"
+# include "libft.h"
+# include "str_cmd_inf.h"
 
 t_list	*split_cmd_semicolon(t_str_cmd_inf *str_cmd_inf);
 
