6d5f851e5c33a7d98a095e0bf93b724cf5bce640
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-01-10 11:36:16 +0100

    Incrementation du curseur si le caractere est echappe

--- src/str_cmd_inf.c
+++ src/str_cmd_inf.c
@@ -6,7 +6,7 @@
 /*   By: fwerner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 12:15:36 by fwerner           #+#    #+#             */
-/*   Updated: 2019/01/10 11:32:14 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/01/10 11:35:16 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,21 +57,22 @@ int				scmd_move_to_next_char(t_str_cmd_inf *str_cmd_inf)
 {
 	if (str_cmd_inf->str[str_cmd_inf->pos] == '\0')
 		return (0);
-	if (scmd_cur_char_is_escaped(str_cmd_inf))
-		return (1);
-	if (str_cmd_inf->is_in_quote)
+	if (!scmd_cur_char_is_escaped(str_cmd_inf))
 	{
-		if (str_cmd_inf->str[str_cmd_inf->pos] == '\'')
-			str_cmd_inf->is_in_quote = 0;
-	}
-	else if (str_cmd_inf->is_in_var_bracket)
-	{
-		if (str_cmd_inf->str[str_cmd_inf->pos] == '}')
-			str_cmd_inf->is_in_var_bracket = 0;
-	}
-	else
-	{
-		move_to_next_char_not_quote_or_var(str_cmd_inf);
+		if (str_cmd_inf->is_in_quote)
+		{
+			if (str_cmd_inf->str[str_cmd_inf->pos] == '\'')
+				str_cmd_inf->is_in_quote = 0;
+		}
+		else if (str_cmd_inf->is_in_var_bracket)
+		{
+			if (str_cmd_inf->str[str_cmd_inf->pos] == '}')
+				str_cmd_inf->is_in_var_bracket = 0;
+		}
+		else
+		{
+			move_to_next_char_not_quote_or_var(str_cmd_inf);
+		}
 	}
 	++(str_cmd_inf->pos);
 	return (1);
