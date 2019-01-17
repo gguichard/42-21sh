f1253c0e75b8725a871a07c62d512a1fe11f4cfa
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-01-17 10:17:03 +0100

    Mise a jour grammaire (pas sur du fonctionnement)

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/11 14:26:58 by gguichar          #+#    #+#             */
-/*   Updated: 2019/01/16 16:14:34 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/01/17 10:13:37 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,18 +41,35 @@ int		expect_token(t_list **curr, t_token_type type)
 	token = "newline";
 	if (*curr != NULL)
 		token = ((t_token_inf *)(*curr)->content)->token;
-	ft_dprintf(2, "%s: syntax error near unexpected token `%c'\n"
-			, ERR_PREFIX, *token);
+	ft_dprintf(2, "%s: syntax error near unexpected token `%.*s'\n"
+			, ERR_PREFIX, *curr != NULL ? 1 : 7, token);
 	return (0);
 }
 
 int		parse_operator(t_list **curr)
 {
+	int	ret;
+
+	ret = 0;
 	if (accept_token(curr, TK_OPE))
-		return (1);
-	if (accept_token(curr, TK_NUM_OPT))
-		return (!expect_token(curr, TK_OPE) ? -1 : 1);
-	return (0);
+		ret = 1;
+	else if (accept_token(curr, TK_NUM_OPT))
+		ret = !expect_token(curr, TK_OPE) ? -1 : 1;
+	return (ret);
+}
+
+int		parse_command_part(t_list **curr)
+{
+	int	ret;
+
+	while (accept_token(curr, TK_WORD))
+		;
+	ret = parse_operator(curr);
+	if (ret < 0)
+		return (-1);
+	if (ret > 0 && !accept_token(curr, TK_STR_OPT))
+		return (!expect_token(curr, TK_WORD) ? -1 : 1);
+	return (ret);
 }
 
 void	parse_commands(t_list *tokens)
@@ -63,16 +80,12 @@ void	parse_commands(t_list *tokens)
 	curr = tokens;
 	if (!expect_token(&curr, TK_WORD))
 		return ;
-	while (accept_token(&curr, TK_WORD))
-	{
-		ret = parse_operator(&curr);
-		if (ret < 0 || (ret > 0 && !expect_token(&curr, TK_WORD)))
-			return ;
-	}
-	ret = parse_operator(&curr);
+	ret = 0;
+	while ((ret = parse_command_part(&curr)) > 0)
+		;
 	if (ret < 0)
 		return ;
-	if (ret > 0 || (accept_token(&curr, TK_CMD_SEP) && curr != NULL))
+	if (accept_token(&curr, TK_CMD_SEP) && curr != NULL)
 		parse_commands(curr);
 	else if (curr != NULL)
 	{
