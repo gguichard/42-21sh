c37af00459088edd6054d7c3f720e26dd0d6466f
Franck W <franck.rj.w@gmail.com>
2019-01-25 11:37:17 +0100

    Ajout d'une fonction de pre-application d'echappement pour les \n.

--- includes/apply_escape.h
+++ includes/apply_escape.h
@@ -6,7 +6,7 @@
 /*   By: fwerner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 08:58:37 by fwerner           #+#    #+#             */
-/*   Updated: 2019/01/18 08:58:39 by fwerner          ###   ########.fr       */
+/*   Updated: 2019/01/25 11:30:46 by fwerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,11 @@
 ** echappes remplaces par les caracteres adequats.
 */
 char	*apply_escape(const char *str);
+
+/*
+** Alloue et retourne une nouvelle chaine de caractere avec les newlines
+** echappes (non presents dans des quotes) supprimes.
+*/
+char	*apply_only_newline_escape(const char *str);
 
 #endif
--- src/parser/apply_escape.c
+++ src/parser/apply_escape.c
@@ -6,7 +6,7 @@
 /*   By: fwerner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 08:57:59 by fwerner           #+#    #+#             */
-/*   Updated: 2019/01/23 16:23:49 by fwerner          ###   ########.fr       */
+/*   Updated: 2019/01/25 11:35:10 by fwerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,13 +75,34 @@ char			*apply_escape(const char *str)
 		if (str_cmd_inf.str[str_cmd_inf.pos] == '\\'
 				&& (scmd_cur_char_is_in_nothing(&str_cmd_inf)
 					|| (str_cmd_inf.is_in_doublequote
-						&& (str_cmd_inf.str[str_cmd_inf.pos + 1] == '$'
-							|| str_cmd_inf.str[str_cmd_inf.pos + 1] == '\"'
-							|| str_cmd_inf.str[str_cmd_inf.pos + 1] == '\\'
-							|| str_cmd_inf.str[str_cmd_inf.pos + 1] == '\n'))))
+						&& ft_strchr("$\"\\\n",
+							str_cmd_inf.str[str_cmd_inf.pos + 1]) != NULL)))
 			remove_cur_char_from_scmd(&str_cmd_inf, new_str, &str_len, 1);
 		else if (need_to_remove_cur_char(&str_cmd_inf))
 			remove_cur_char_from_scmd(&str_cmd_inf, new_str, &str_len, 0);
+		else
+			++(str_cmd_inf.pos);
+	}
+	scmd_delete(str_cmd_inf.sub_var_bracket);
+	return (new_str);
+}
+
+char			*apply_only_newline_escape(const char *str)
+{
+	t_str_cmd_inf	str_cmd_inf;
+	char			*new_str;
+	size_t			str_len;
+
+	str_len = ft_strlen(str);
+	if ((new_str = ft_strndup(str, str_len)) == NULL)
+		return (NULL);
+	scmd_init(&str_cmd_inf, new_str);
+	while (str_cmd_inf.str[str_cmd_inf.pos] != '\0')
+	{
+		if (str_cmd_inf.str[str_cmd_inf.pos] == '\\'
+				&& !str_cmd_inf.is_in_quote && !str_cmd_inf.is_in_doublequote
+				&& !scmd_cur_char_is_escaped(&str_cmd_inf))
+			remove_cur_char_from_scmd(&str_cmd_inf, new_str, &str_len, 1);
 		else
 			++(str_cmd_inf.pos);
 	}
