524dc802907f3e6c3693ce67050afcf4d5b42e62
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-01-29 18:08:18 +0100

    Fix leaks 2

--- src/misc/hashtable_utils.c
+++ src/misc/hashtable_utils.c
@@ -6,7 +6,7 @@
 /*   By: fwerner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 15:59:54 by fwerner           #+#    #+#             */
-/*   Updated: 2019/01/28 16:05:46 by fwerner          ###   ########.fr       */
+/*   Updated: 2019/01/29 18:07:10 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,38 +48,4 @@ t_hashtable		*make_def_hashtable(void)
 	new_hashtable->hash_fun = def_hash_fun;
 	new_hashtable->del_hashentry_val_fun = def_del_hashentry_val_fun;
 	return (new_hashtable);
-}
-
-void			delete_hashtable(t_hashtable *hashtable)
-{
-	size_t	bucket_idx;
-	t_list	*cur_bucket;
-	t_list	*old_bucket;
-
-	bucket_idx = 0;
-	while (bucket_idx < hashtable->bucket_count)
-	{
-		cur_bucket = hashtable->buckets[bucket_idx];
-		while (cur_bucket != NULL)
-		{
-			delete_hashentry(hashtable, (t_hashentry*)cur_bucket->content);
-			old_bucket = cur_bucket;
-			cur_bucket = cur_bucket->next;
-			free(old_bucket);
-		}
-		++bucket_idx;
-	}
-	free(hashtable->buckets);
-	free(hashtable);
-}
-
-int				delete_hashentry(t_hashtable *hashtable, t_hashentry *entry)
-{
-	if (entry != NULL)
-	{
-		free(entry->key);
-		hashtable->del_hashentry_val_fun(entry->value, entry->value_size);
-		free(entry);
-	}
-	return (0);
 }
