a43df473ee6c45cba7fed84d24a5ba9e925fe103
Gwennael Guichard <gguichar@student.42.fr>
2019-01-29 23:21:49 +0100

    Changement get_var par get_shell_var

--- src/builtins/change_dir.c
+++ src/builtins/change_dir.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/23 15:13:49 by gguichar          #+#    #+#             */
-/*   Updated: 2019/01/29 09:48:01 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/01/29 23:18:58 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ static t_opts	*cd_get_opts(int argc, char **argv)
 	opts = parse_opts(argc, argv, "");
 	if (opts == NULL)
 	{
-		ft_dprintf(2, "%s: %s: unexpected error\n", ERR_PREFIX, argv[0]);
+		ft_dprintf(2, "%s: %s: Unexpected error\n", ERR_PREFIX, argv[0]);
 		return (NULL);
 	}
 	else if (opts->error != 0)
 	{
-		ft_dprintf(2, "%s: %s: -%c: invalid option\n", ERR_PREFIX, argv[0]
+		ft_dprintf(2, "%s: %s: -%c: Invalid option\n", ERR_PREFIX, argv[0]
 				, opts->error);
 		ft_dprintf(2, "%s: usage: cd [dir]\n", argv[0]);
 		ft_memdel((void *)&opts);
@@ -39,9 +39,9 @@ static t_opts	*cd_get_opts(int argc, char **argv)
 	return (opts);
 }
 
-static char		*get_full_cur_path(t_shell *shell, const char *cur_path)
+static char		*get_new_cur_path(t_shell *shell, const char *cur_path)
 {
-	t_var	*pwd;
+	char	*pwd;
 	size_t	pwd_len;
 	size_t	cur_len;
 	char	*new_path;
@@ -50,21 +50,21 @@ static char		*get_full_cur_path(t_shell *shell, const char *cur_path)
 		new_path = ft_strdup(cur_path);
 	else
 	{
-		pwd = get_var(shell->env, "PWD");
-		pwd_len = (pwd == NULL) ? 0 : ft_strlen(pwd->value);
-		if (pwd_len > 0 && (pwd->value)[pwd_len - 1] == '/')
+		pwd = get_shell_var(shell, "PWD");
+		pwd_len = (pwd == NULL) ? 0 : ft_strlen(pwd);
+		if (pwd_len > 0 && pwd[pwd_len - 1] == '/')
 			pwd_len -= 1;
 		cur_len = ft_strlen(cur_path);
 		new_path = (char *)malloc((pwd_len + cur_len + 2) * sizeof(char));
-		if (new_path == NULL)
-			return (NULL);
-		if (pwd != NULL)
-			ft_memcpy(new_path, pwd->value, pwd_len);
-		new_path[pwd_len] = '/';
-		ft_memcpy(new_path + pwd_len + 1, cur_path, cur_len + 1);
+		if (new_path != NULL)
+		{
+			if (pwd != NULL)
+				ft_memcpy(new_path, pwd, pwd_len);
+			new_path[pwd_len] = '/';
+			ft_memcpy(new_path + pwd_len + 1, cur_path, cur_len + 1);
+		}
+		free(pwd);
 	}
-	if (new_path != NULL)
-		set_dir_to_canonical_form(new_path);
 	return (new_path);
 }
 
@@ -72,7 +72,6 @@ static char		*get_cur_path(t_shell *shell, t_opts *opts, int argc
 		, char **argv)
 {
 	char	*cur_path;
-	t_var	*old_pwd;
 	char	*tmp;
 
 	cur_path = NULL;
@@ -81,15 +80,14 @@ static char		*get_cur_path(t_shell *shell, t_opts *opts, int argc
 	else if (!ft_strequ(argv[opts->index], "-"))
 		cur_path = ft_strdup(argv[opts->index]);
 	else
-	{
-		old_pwd = get_var(shell->env, "OLDPWD");
-		cur_path = old_pwd == NULL ? NULL : ft_strdup(old_pwd->value);
-	}
+		cur_path = get_shell_var(shell, "OLDPWD");
 	if (cur_path != NULL)
 	{
-		tmp = get_full_cur_path(shell, cur_path);
+		tmp = get_new_cur_path(shell, cur_path);
 		free(cur_path);
 		cur_path = tmp;
+		if (cur_path != NULL)
+			set_dir_to_canonical_form(cur_path);
 	}
 	return (cur_path);
 }
@@ -128,7 +126,7 @@ int				builtin_cd(t_shell *shell, int argc, char **argv)
 	if (cur_path == NULL)
 	{
 		ret = 1;
-		ft_dprintf(2, "%s: %s: unexpected error\n", ERR_PREFIX, argv[0]);
+		ft_dprintf(2, "%s: %s: Unexpected error\n", ERR_PREFIX, argv[0]);
 	}
 	else
 	{
