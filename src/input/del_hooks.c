5b64cc6a783869f79e4754f39d7b4236f16b68ef
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-01-29 13:20:27 +0100

    Fix invalid read

--- src/input/del_hooks.c
+++ src/input/del_hooks.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/04 16:06:26 by gguichar          #+#    #+#             */
-/*   Updated: 2019/01/23 14:10:44 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/01/29 13:14:23 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 
 static void	del_on_cursor(t_shell *shell, t_term *term)
 {
-	ft_memmove(&(term->line[term->cursor])
-			, &(term->line[term->cursor + 1])
-			, term->size - term->cursor + 1);
+	if (term->cursor < term->size)
+		ft_memmove(term->line + term->cursor, term->line + term->cursor + 1
+				, term->size - term->cursor);
 	(term->size)--;
 	go_to_prompt(term);
 	print_input(shell, term);
