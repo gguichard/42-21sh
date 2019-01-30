bce0f099dee276c590d177dc3c9bd9f500485982
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-01-30 12:04:03 +0100

    Interception SIGKILL

--- src/misc/signals.c
+++ src/misc/signals.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 10:03:17 by gguichar          #+#    #+#             */
-/*   Updated: 2019/01/30 09:27:14 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/01/30 12:01:40 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,18 @@ extern t_shell	*g_shell;
 static void		clean_exit(int sig)
 {
 	reset_term(g_shell);
+	ft_putendl_fd("exit", 2);
 	exit(sig);
 }
 
-void			kill_forked_pids(t_shell *shell)
+void			kill_forked_pids(t_shell *shell, int sig)
 {
 	t_list	*curr;
 
 	curr = shell->fork_pids;
 	while (curr != NULL)
 	{
-		kill(*((pid_t *)curr->content), SIGINT);
+		kill(*((pid_t *)curr->content), sig);
 		curr = curr->next;
 	}
 }
@@ -39,11 +40,13 @@ static void		handle_signal(int sig)
 {
 	if (sig == SIGWINCH)
 		update_winsize(&(g_shell->term));
-	if (sig == SIGINT)
+	else if (sig == SIGQUIT)
+		kill_forked_pids(g_shell, SIGINT);
+	else if (sig == SIGINT)
 	{
 		if (g_shell->fork_pids != NULL)
 		{
-			kill_forked_pids(g_shell);
+			kill_forked_pids(g_shell, SIGINT);
 			ft_putchar_fd('\n', 2);
 		}
 		else
@@ -62,8 +65,8 @@ void			setup_signals(void)
 	signal(SIGHUP, clean_exit);
 	signal(SIGTERM, clean_exit);
 	signal(SIGINT, clean_exit);
-	signal(SIGQUIT, clean_exit);
 	signal(SIGCONT, clean_exit);
+	signal(SIGQUIT, handle_signal);
 	signal(SIGWINCH, handle_signal);
 	signal(SIGINT, handle_signal);
 	signal(SIGTSTP, handle_signal);
