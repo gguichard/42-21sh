e5b268992922b3706193f71670beb5c226ed6d22
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-01-30 13:13:58 +0100

    Fix redirection input

--- src/exec/redir_input.c
+++ src/exec/redir_input.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 12:06:28 by gguichar          #+#    #+#             */
-/*   Updated: 2019/01/29 12:17:09 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/01/30 13:13:23 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,12 @@
 #include <fcntl.h>
 #include "execute.h"
 
-int	redirect_input(t_redirect_inf *redirect_inf)
+static int	get_redirect_input_fd(t_redirect_inf *redirect_inf)
 {
 	int	fd;
-	int	should_close;
 
 	fd = redirect_inf->to_fd;
-	should_close = redirect_inf->close_to_fd;
-	if (fd == FD_DEFAULT)
-		fd = redirect_inf->from_fd;
-	else if (fd == FD_NOTSET)
+	if (fd == FD_NOTSET)
 	{
 		fd = open(redirect_inf->to_word, O_RDONLY);
 		should_close = 1;
@@ -34,10 +30,34 @@ int	redirect_input(t_redirect_inf *redirect_inf)
 				, redirect_inf->to_word);
 		return (0);
 	}
-	if (fd < 0)
-		return (0);
-	dup2_with_rc(redirect_inf, fd, ft_max(STDIN_FILENO, redirect_inf->from_fd));
+	return (fd);
+}
+
+int			redirect_input(t_redirect_inf *redirect_inf)
+{
+	int	from_fd;
+	int	to_fd;
+	int	should_close;
+	int	ret;
+
+	from_fd = redirect_inf->from_fd;
+	if (from_fd == FD_DEFAULT)
+		from_fd = 0;
+	should_close = redirect_inf->close_to_fd;
 	if (should_close)
-		close(fd);
-	return (1);
+	{
+		close(from_fd);
+		return (1);
+	}
+	if ((to_fd = get_redirect_input_fd(redirect_inf)) < 0)
+		return (0);
+	ret = 0;
+	if (from_fd != to_fd)
+	{
+		if (!(ret = dup2_with_rc(redirect_inf, to_fd, from_fd)))
+			ft_dprintf(2, "%s: %d: Bad file descriptor\n", ERR_PREFIX, to_fd);
+	}
+	if (should_close)
+		close(to_fd);
+	return (ret);
 }
